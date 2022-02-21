10e57558398497c8b7eca4158f9440c0465a7956
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2022-02-20 21:02:06 -0800

    Try working with weather api, didn't get it up

--- src/frontend/my-app/src/App.js
+++ src/frontend/my-app/src/App.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import {Navbar, Home, News, Tasklist, Createtask, About} from './Page';
-import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import { Navbar, Home, News, Tasklist, Createtask, About, Weather } from './Page';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route path="/news" element={<News/>} />
-        <Route path="/tasklist" element={<Tasklist/>} />
-        <Route path="/createtask" element={<Createtask/>} />
-        <Route path="/about" element={<About/>} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Navbar />
+        <Weather />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/tasklist" element={<Tasklist />} />
+          <Route path="/createtask" element={<Createtask />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
