af8d4de39be622b9e8aabd6441e23d0d5d5f679d
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2022-02-20 16:36:12 -0800

    create nav bar

---
+++ src/frontend/my-app/src/Page/navbar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginTop: theme.spacing(1),
+    display: "flex",
+  },
+ logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(10),
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" className={classes.logo}>
+          Navbar
+        </Typography>
+          <div className={classes.navlinks}>
+            <Link to="/" className={classes.link}>
+              Home
+            </Link>
+            <Link to="/createtask" className={classes.link}>
+              Createtask
+            </Link>
+            <Link to="/tasklist" className={classes.link}>
+              Task List
+            </Link>
+            <Link to="/news" className={classes.link}>
+              News
+            </Link>
+            <Link to="/about" className={classes.link}>
+              FAQ
+            </Link>
+          </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default Navbar;
