d2af1cecf5160262baf8e221a377d2569452db05
Zhengmao Zhang <zhengmao@pdx.edu>
2022-02-23 03:28:01 -0800

    update submit event page

--- src/frontend/my-app/src/Page/calendar.js
+++ src/frontend/my-app/src/Page/calendar.js
@@ -1,10 +1,79 @@
-import React from "react";
-
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Grid from '@material-ui/core/Grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import purple from '@material-ui/core/colors/purple';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import red from '@material-ui/core/colors/red';
+const cards = [
+    {id: 1, startTime: '2022/03/09 0:00', endTime: '2022/03/11 0:00',location: 'Portland',description: 'Final Project Presentation', statu : "Down"},
+    {id: 2, startTime: '2022/03/09 0:00', endTime: '2022/03/11 0:00',location: 'Portland',description: 'Final Project Presentation', statu : "Down"},
+    {id: 3, startTime: '2022/03/09 0:00', endTime: '2022/03/11 0:00',location: 'Portland',description: 'Final Project Presentation', statu : "Down"},
+    {id: 4, startTime: '2022/03/09 0:00', endTime: '2022/03/11 0:00',location: 'Portland',description: 'Final Project Presentation', statu : "Down"},
+    {id: 5, startTime: '2022/03/09 0:00', endTime: '2022/03/11 0:00',location: 'Portland',description: 'Final Project Presentation', statu : "Down"},
+];
+const theme = createTheme({
+    palette: {
+      primary: {
+        // Purple and green play nicely together.
+        main: '#42a5f5',
+      },
+      secondary: {
+        // This is green.A700 as hex.
+        main: '#d32f2f',
+      },
+    },
+  });
+const weather = "Portland, Oregon, United States: ☀️   +28°F";
 function Calendar() {
     return(
+        <main>
         <div>
-            <h1>Google Calendar</h1>
+            <Grid container spacing={40}>
+                {cards.map(card => (
+                    <Grid item key={card} sm={4} md={4} lg={4}>
+                        <Card >
+                            <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image="https://www.galchimia.com/wp-content/uploads/2021/06/how-to-crash-your-own-presentation.jpg"
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="div">
+                                {card.startTime}
+                                </Typography>
+                                <Typography gutterBottom variant="h6" component="div">
+                                {card.endTime}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                {weather}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                {card.description}
+                                </Typography>
+                            </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                            <ThemeProvider theme={theme}>
+                                <Button size="small" color="primary">
+                                    Edit
+                                </Button>
+                                <Button size="small" color="secondary">
+                                    Delete
+                                </Button>
+                            </ThemeProvider>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </div>
+        </main>
     )
 }
 export default Calendar;
--- src/frontend/my-app/src/Page/createtask.js
+++ src/frontend/my-app/src/Page/createtask.js
@@ -1,10 +1,98 @@
-import React from "react";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { DateTimePicker } from "@material-ui/pickers";
+import { useEffect, useState } from "react";
+const theme = createTheme();
 
-function Createtask() {
-    return(
-        <div>
-            <h1>CREATE TASK</h1>
-        </div>
-    )
+export default function SignUp() {
+  const handleSubmit = (event) => {
+  };
+  const [selectedDate, handleDateChange] = useState(new Date());
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar> */}
+          <Typography component="h1" variant="h5">
+            Add Event
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={3}>
+            <Grid item xs={6}>
+                Start Date:
+                <TextField
+                  required
+                  fullWidth
+                  name="startDate"
+                //   label="startDate"
+                  type="date"
+                  id="startdate"
+                />
+              </Grid>
+              <Grid item xs={6}>
+              End Date:
+                <TextField
+                  required
+                  fullWidth
+                //   label="endDate"
+                  name="endDate"
+                  type="date"
+                  id="enddate"
+                />
+              </Grid>
+              <Grid item xs={12}>
+              {/* Location: */}
+                <TextField
+                  fullWidth
+                  name="location"
+                  label="Location"
+                  type="location"
+                  id="location"
+                />
+              </Grid>
+              <Grid item xs={12} style={{height: 200}}>
+                {/* Description: */}
+                <TextField
+                  fullWidth
+                  name="description"
+                  label="Description"
+                  type="text"
+                  id="description"
+                  
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
-export default Createtask;
