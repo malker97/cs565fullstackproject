d7735bc5c136679dbddc039cc7f9c6bdd40698e7
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2022-02-23 18:43:09 -0800

    create redirect and stored data

--- src/frontend/my-app/src/Page/home.js
+++ src/frontend/my-app/src/Page/home.js
@@ -2,66 +2,66 @@ import React, { useState, useEffect } from "react";
 import { TextField, Button } from "@material-ui/core";
 import '../Style/home.css';
 import { makeStyles } from "@material-ui/core";
-import { createStore, action } from "easy-peasy";
+import { createStore, action, useStoreActions, useStoreState } from "easy-peasy";
 import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
-    margin: {
-      marginTop: theme.spacing(8),
-      marginLeft: "20%",
-      width: "20%",
-    },
-    extendedIcon: {
-      marginRight: theme.spacing(1),
-    },
-  }));
+  margin: {
+    marginTop: theme.spacing(8),
+    marginLeft: "20%",
+    width: "20%",
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
-const store = createStore({
-  loggedIn: false,
-  login: action((state, newstatus) => {
-    state.loggedIn = newstatus;
-  }),
-});
+
 
 function Home() {
-    const classes = useStyles();
-    const [userid, setuserid] = useState('');
-   
-    const get_info = () => {
-      axios
-        .get(
-          ``
-        )
-        .then((res) => {
-          console.log(res)
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+  const classes = useStyles();
+  const [userid, setuserid] = useState('');
+  const login = useStoreActions((actions) => actions.login);
 
-    useEffect(()=>{
-      if(userid!==''){
-        
-      }
-    },[userid])
+  const loggedIn = useStoreState((state) => state.loggedIn);
+
+  const get_info = () => {
+    axios
+      .get(
+        ``
+      )
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+
+    if (userid !== '') {
+      
+    }
+    console.log(loggedIn);
+  }, [userid])
 
 
-    return (
-        <div className="container">
-            <form className="taskid" noValidate autoComplete="off">
-                <TextField
-                    id="standard-basic"
-                    label="Please enter your user id/task id"
-                    aria-describedby="my-helper-text"
-                    className="idinput"
-                    onChange={e => {setuserid(e.target.value); console.log(e.target.value);}}
-                />
-                <br/>
-                <Button className={classes.margin} variant="contained" color="primary" onClick={get_info()}>Submit</Button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="container">
+      <form className="taskid" noValidate autoComplete="off">
+        <TextField
+          id="standard-basic"
+          label="Please enter your user id/task id"
+          aria-describedby="my-helper-text"
+          className="idinput"
+          onChange={e => { setuserid(e.target.value); console.log(e.target.value); }}
+        />
+        <br />
+        <Button className={classes.margin} variant="contained" color="primary" onClick={e => {get_info(); login(true);}}>Submit</Button>
+      </form>
+    </div>
+  )
 }
 
 export default Home;
--- src/frontend/my-app/src/index.js
+++ src/frontend/my-app/src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, StoreProvider, action } from "easy-peasy";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider store={createStore({
+      loggedIn: false,
+      login: action((state, newstatus) => {
+        state.loggedIn = newstatus;
+      }),
+      userid: '',
+      setid: action((state, newstatus) => {
+        state.userid = newstatus;
+      }),
+      newuser: false,
+      setnewuser: action((state, newstatus) => {
+        state.newuser = newstatus;
+      }),
+    })}>
+      <App />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
