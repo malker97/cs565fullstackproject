2b9d0bb577a961d1a6354f84c4f295f4a8a17140
Zhengmao Zhang <zhengmao@pdx.edu>
2022-02-23 17:36:47 -0800

    refactor task list file

--- src/frontend/my-app/src/Page/tasklist.js
+++ src/frontend/my-app/src/Page/tasklist.js
@@ -1,112 +1,60 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
-import Popover from '@material-ui/core/Popover';
-import { Typography } from "@material-ui/core";
-import Data from '../exampled_json/tasklist.json';
+import { DataGrid } from '@mui/x-data-grid';
+const columns = [
+  { field: 'id', 
+  headerName: 'EventID', 
+  width: 90 },
+  {
+    field: 'starttime',
+    headerName: 'Start Time',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'endtime',
+    headerName: 'End Time',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'location',
+    headerName: 'Location',
+    type: 'text',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'description',
+    headerName: 'Description',
+    // description: 'This column has a value getter and is not sortable.',
+    type: 'text',
+    sortable: false,
+    width: 240,
+  },
+];
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '70%',
-    marginTop: "5%",
-    marginLeft: "10%",
-    backgroundColor: theme.palette.background.paper,
-  },
-  popover: {
-    pointerEvents: 'none',
-  },
-  paper: {
-    padding: theme.spacing(1),
-  },
-}));
-
+const rows = [
+  { id: 1, starttime: '2022/03/09 0:00', endtime: '2022/03/09 0:00', location: 'Portland', description: 'Final Project Presentation' },
+  { id: 2, starttime: '2022/03/09 0:00', endtime: '2022/03/09 0:00', location: 'Portland', description: 'Final Project Presentation' },
+  { id: 3, starttime: '2022/03/09 0:00', endtime: '2022/03/09 0:00', location: 'Portland', description: 'Final Project Presentation' },
+  { id: 4, starttime: '2022/03/09 0:00', endtime: '2022/03/09 0:00', location: 'Portland', description: 'Final Project Presentation' },
+  { id: 5, starttime: '2022/03/09 0:00', endtime: '2022/03/09 0:00', location: 'Portland', description: 'Final Project Presentation' },
+  { id: 6, starttime: '2022/03/09 0:00', endtime: '2022/03/09 0:00', location: 'Portland', description: 'Final Project Presentation' },
+  { id: 7, starttime: '2022/03/09 0:00', endtime: '2022/03/09 0:00', location: 'Portland', description: 'Final Project Presentation' },
+];
 function Tasklist() {
-  const classes = useStyles();
-  const [checked, setChecked] = React.useState([0]);
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
 
   return (
-    <List className={classes.root}>
-      {Data.map((value) => {
-        const labelId = `checkbox-list-label-${value.id}`;
-
-        return (
-          <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
-            <ListItemIcon>
-              <Checkbox
-                edge="start"
-                checked={checked.indexOf(value) !== -1}
-                tabIndex={-1}
-                disableRipple
-                inputProps={{ 'aria-labelledby': labelId }}
-              />
-            </ListItemIcon>
-            <ListItemText id={labelId} primary={`${value.title}`} />
-            <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="comments"aria-owns={open ? 'mouse-over-popover' : undefined}
-                  aria-haspopup="true"
-                  onMouseEnter={handlePopoverOpen}
-                  onMouseLeave={handlePopoverClose}>
-                <CommentIcon />
-              </IconButton>
-              <Popover
-                  id="mouse-over-popover"
-                  className={classes.popover}
-                  classes={{
-                    paper: classes.paper,
-                  }}
-                  open={open}
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                  onClose={handlePopoverClose}
-                  disableRestoreFocus
-                >
-                  {/* Not working */}
-                  <Typography>Popover didn't work!!!!!</Typography>
-                </Popover>
-            </ListItemSecondaryAction>
-          </ListItem>
-        );
-      })}
-    </List>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
   );
 }
 export default Tasklist;
